46ddab18adf28e388efcd9dddbecfaa1f2312cdb
Yuriy Bodolanov <magikonermad@yandex.ru>
2023-05-19 15:34:03 +0100

    поправил бэк под входящие данные с beatfilm-movies

--- middlewares/rateLimiter/limit.js
+++ middlewares/rateLimiter/limit.js
@@ -1,6 +1,6 @@
 const limit = {
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   standardHeaders: true,
   legacyHeaders: false,
 };
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 const modelLinkPattern = /^(ftp|http|https):\/\/[^ "]+$/;
-const linkPattern = /^(ftp|http|https):\/\/[^"]+\.\w{2,}/;
+// const linkPattern = /^(ftp|http|https):\/\/[^"]+\.\w{2,}/;
+const linkPattern = /[a-zA-A0-9_]+\.\w{2,}/;
 const modelEmailPatter = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // MESSAGES
